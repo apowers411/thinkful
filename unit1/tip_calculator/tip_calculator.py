681ae0de3889e8c7cb4aaeb104e6c591bea1dde9
Alicia <apowers411@yahoo.com>
2013-11-23 10:27:49 -0500

    cleaner

--- unit1/tip_calculator/tip_calculator.py
+++ unit1/tip_calculator/tip_calculator.py
@@ -1,20 +1,16 @@
 #! /usr/bin/env python
 
-meal=raw_input("Enter meal")
-tax=raw_input("Enter tax")
-tip=raw_input("Enter tip")
-
-meal=float(meal)
-tax=float(tax)
-tip=float(tip)
+meal=float(raw_input("Enter meal"))
+tax=float(raw_input("Enter tax"))
+tip=float(raw_input("Enter tip"))
 
 tax_value=meal*tax
 meal_with_tax=meal+tax_value
 tip_value=meal*tip
 total=meal_with_tax+tip_value
 
-print("%.2f" % meal)
-print("%.2f" % tax_value)
-print("%.2f" % tip_value)
-print("%.2f" % total)
+print("This is the meal %.2f" % meal)
+print("This is the tax value %.2f" % tax_value)
+print("This is the tip_value %.2f" % tip_value)
+print("This is the total %.2f" % total)
 
