b930af70236f6e3d6eab142788b9b77fa1667aca
Alicia <apowers411@yahoo.com>
2013-11-23 10:51:28 -0500

    added options

--- unit1/tip_calculator/tip_calculator.py
+++ unit1/tip_calculator/tip_calculator.py
@@ -1,18 +1,23 @@
 #! /usr/bin/env python
 import sys
+from optparse import OptionParser
 
-print ("enter meal then tax then tip")
+mine=OptionParser()
+mine.add_option("-m","--meal", dest="meal",help="meal cost",type="float")
+mine.add_option("-x","--tax", dest="tax",help="tax cost",default=.12,type="float")
+mine.add_option("-p","--tip", dest="tip",help="tip cost",type="float")
 
-meal=float(sys.argv[1])
-tax=float(sys.argv[2])
-tip=float(sys.argv[3])
+(options, args)=mine.parse_args()
 
-tax_value=meal*tax
-meal_with_tax=meal+tax_value
-tip_value=meal*tip
+if not (options.meal or options.tip):
+    parser.error("You need to supple an argument for -tip or -meal ")
+
+tax_value=options.meal*options.tax
+meal_with_tax=options.meal+tax_value
+tip_value=options.meal*options.tip
 total=meal_with_tax+tip_value
 
-print("This is the meal %.2f" % meal)
+print("This is the meal %.2f" % options.meal)
 print("This is the tax value %.2f" % tax_value)
 print("This is the tip_value %.2f" % tip_value)
 print("This is the total %.2f" % total)
