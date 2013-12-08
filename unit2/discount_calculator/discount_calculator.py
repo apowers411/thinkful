00f30a9e98544fe0cf8de03d7f83094d41a878f6
Alicia <apowers411@yahoo.com>
2013-12-08 16:18:55 -0500

    Milestone6

--- unit2/discount_calculator/discount_calculator.py
+++ unit2/discount_calculator/discount_calculator.py
@@ -1,9 +1,12 @@
 class DiscountCalculator(object):
     def calculate(self, total, discount_amount,discount_type):
          if discount_type=="percent":
+             if discount_amount>100:
+                 raise ValueError("Percentage discount cannot exceed 100%)
              discount=float(total)*float(discount_amount)/100
-            
          elif discount_type == 'absolute': 
+             if discount_amount>total:
+                raise ValueError("absolute discount cannot exceed order total")
              discount=discount_amount
          else: 
              raise ValueError("Invalid discount type")
--- unit2/discount_calculator/discount_calculator_test.py
+++ unit2/discount_calculator/discount_calculator_test.py
@@ -30,5 +30,13 @@ class DiscountCalculatorTests(unittest.TestCase):
         result= discount_calculator.calculate(250.0, 5.0, 'absolute')
         self.assertEqual(5.0, result)
 
+    def excessive_discount_type_test(self):
+        discount_calculator=DiscountCalculator()
+        self.assertRaises(ValueError, discount_calculator.calculate, 250,110,'percent')
+
+    def excessive_absolute_discount_test(self):
+        discount_calculator=DiscountCalculator()
+        self.assertRaises(ValueError, discount_calculator.calculate,250,260,'absolute')
+
 
     
